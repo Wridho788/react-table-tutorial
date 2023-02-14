f6309ad5a3f7a68e0f8d6215eb62521bada11a8a
Wridho788 <wridho246@gmail.com>
2023-02-14 16:48:03 +0700

    add basic table

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,23 +1,43 @@
 import React, { useMemo } from 'react';
 import MOCK_DATA from './MOCK_DATA.json';
-import { COLUMN } from './columns';
+import { COLUMNS } from './columns';
 import { useTable } from 'react-table';
+import './style.css';
 
 const BasicTable = () => {
-    const columns = useMemo(() => COLUMN, []);
+    const columns = useMemo(() => COLUMNS, []);
     const data = useMemo(() => MOCK_DATA, []);
 
-    const tableInstance = useTable({ columns, data });
+    const tableInstance = useTable({ column: COLUMNS, data: MOCK_DATA });
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        tableInstance;
 
     return (
-        <table>
+        <table {...getTableProps()}>
             <thead>
-                <tr></tr>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderGroupProps()}>
+                                {column.render('Header')}
+                            </th>
+                        ))}
+                        <th></th>
+                    </tr>
+                ))}
             </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                </tr>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                            })}
+                        </tr>
+                    );
+                })}
             </tbody>
         </table>
     );
